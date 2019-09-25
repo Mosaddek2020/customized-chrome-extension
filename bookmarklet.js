5b8b645536aa9082cb51a9e8a8351eb33dcf07e8
Mosaddek2020 <mosaddek2020@gmail.com>
2019-09-26 02:05:58 +0600

    bookmarklet creation version 2.0

---
+++ bookmarklet.js
@@ -0,0 +1,11 @@
+(function(){
+
+    var allParags = document.getElementsByTagName('p');
+    for (let i = 0; i < allParags.length; i++) {
+            //allParags[i].innerHTML = 'Mosaddek';
+            allParags[i].style['background-color'] = '#C0C';
+        
+     }
+
+})();
+
