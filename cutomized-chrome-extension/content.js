9a7cba50f9cc79584d57cbc454146f735c04f221
Mosaddek2020 <mosaddek2020@gmail.com>
2019-09-26 18:51:49 +0600

    customized chrome extension version 1.0.. color all <p>

---
+++ cutomized-chrome-extension/content.js
@@ -0,0 +1,13 @@
+(function(){
+
+    var all_para = document.getElementsByTagName('p');
+    
+
+    for (var i = 0; i < all_para.length; i++) {
+
+        all_para[i].style['background-color'] = '#C0C';
+        
+    }
+
+
+})();
